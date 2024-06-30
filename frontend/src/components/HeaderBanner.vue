78849aa8b3b97c7a2c5f30d44847adcbcac37e70
CurvaXs <suxarik24@gmail.com>
2024-06-30 03:00:39 +0300

    make slider

---
+++ frontend/src/components/HeaderBanner.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="header__banner-wrapper">
+        <div class="banner__info">
+            <p>Lorem ipsum dolor sit amet</p>
+            <h1>Lorem ipsum dolor sit amet, consectetur</h1>
+            <button type="button">Lorem suka Ipsum</button>
+        </div>
+        <div class="banner__img">
+            <img src="../assets/img/header__background-ETH.png" alt="">
+            <img src="../assets/img/header__background-avatar.png" alt="">
+        </div>
+    </div>
+</template>
---
+++ frontend/src/components/MainSlider.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="er">
+        <div class="swiper">
+            <!-- Additional required wrapper -->
+            <div class="swiper-wrapper">
+                <!-- Slides -->
+                <div class="swiper-slide">Slide 1</div>
+                <div class="swiper-slide">Slide 2</div>
+                <div class="swiper-slide">Slide 3</div>
+                ...
+            </div>
+            <!-- If we need pagination -->
+            <div class="swiper-pagination"></div>
+
+            <!-- If we need navigation buttons -->
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+
+            <!-- If we need scrollbar -->
+            <div class="swiper-scrollbar"></div>
+        </div>
+    </div>
+
+
+
+</template>
+
+<script setup>
+
+import { onMounted } from 'vue';
+import Swiper from 'swiper/bundle';
+
+onMounted(() => {
+    document.addEventListener('DOMContentLoaded', () => {
+        const swiper = new Swiper('.swiper', {
+            // Optional parameters
+            direction: 'vertical',
+            loop: true,
+
+            // If we need pagination
+            pagination: {
+                el: '.swiper-pagination',
+            },
+
+            // Navigation arrows
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+
+            // And if we need scrollbar
+            scrollbar: {
+                el: '.swiper-scrollbar',
+            },
+        });
+    });
+});
+</script>
+
+<style scoped>
+.er {
+    width: 700px;
+    margin: 0 auto;
+}
+
+.swiper {
+    width: 600px;
+    height: 300px;
+}
+
+.swiper-slide {
+    background-color: red;
+    width: 500px;
+    height: 250px;
+}
+</style>
