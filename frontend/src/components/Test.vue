5f7bdfad753ecc5ab7b7adef8956400c7275ffad
CurvaXs <suxarik24@gmail.com>
2024-07-25 11:40:16 +0300

    make stripe animation

--- frontend/src/components/Test.vue
+++ frontend/src/components/Test.vue
@@ -1,28 +1,28 @@
 <template>
   <div class="strp">
-    <div class="strp__item purple">
-      <p>
-        ONLY $WAY
-        <span class="border-word">PRESALE</span>
-        ONLY $WAY
-        <span class="border-word">PRESALE</span>
-        ONLY $WAY
-        <span class="border-word">PRESALE</span>
-        ONLY $WAY
-        <span class="border-word">PRESALE</span>
-        ONLY $WAY
-      </p>
-    </div>
     <div class="strp__item blue c-white">
       <p>
         ONLY $WAY
-        <span class="border-word">PRESALE</span>
+        <img src="../assets/img/PRESALE.png" alt="">
         ONLY $WAY
-        <span class="border-word">PRESALE</span>
+        <img src="../assets/img/PRESALE.png" alt="">
         ONLY $WAY
-        <span class="border-word">PRESALE</span>
+        <img src="../assets/img/PRESALE.png" alt="">
         ONLY $WAY
-        <span class="border-word">PRESALE</span>
+        <img src="../assets/img/PRESALE.png" alt="">
+        ONLY $WAY
+      </p>
+    </div>
+    <div class="strp__item purple">
+      <p>
+        ONLY $WAY
+        <span class="border-word" data-text="PRESALE"> PRESALE </span>
+        ONLY $WAY
+        <span class="border-word" data-text="PRESALE"> PRESALE </span>
+        ONLY $WAY
+        <span class="border-word" data-text="PRESALE"> PRESALE </span>
+        ONLY $WAY
+        <span class="border-word" data-text="PRESALE"> PRESALE </span>
         ONLY $WAY
       </p>
     </div>
@@ -34,18 +34,47 @@
   width: 100%;
 
   .strp__item {
+    width: 120%;
+
+    margin-left: -15px;
+
     color: #000;
     font-family: 'Inter';
     font-weight: 700;
     font-size: 32px;
+
+    p {
+      padding: 10px 0;
+
+      // .border-word {
+      //   color: #7F35FF;
+      //   position: relative;
+      //   display: inline-block;
+      // }
+
+      // .border-word::before {
+      //   content: attr(data-text);
+      //   position: absolute;
+      //   left: 0;
+      //   top: 0;
+      //   color: black;
+      //   z-index: -1;
+      //   filter: blur(2px);
+      //   opacity: 1;
+      // }
+    }
   }
 
   .purple {
     background-color: #7F35FF;
+    transform: rotate(5deg);
+    transform-origin: left;
   }
 
   .blue {
     background-color: #388BFF;
+    transform: rotate(-5deg);
+    transform-origin: right;
   }
 
   .c-white {
